c87ee311387fcbcac6179698accaa353c986017b
Rxwestx <wexriexst13@gmail.com>
2026-07-17 23:36:20 +0900

    検索用routeを追加し、管理者用routeのコメントを整理

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    // 検索用route
+    Route::get('/search',function () {
+        return inertia('search');
+    })->name('search');
+
+    // 管理者用route
+    // Route::prefix('admin')
+    // ->name('admin.')
+    // ->middleware('admin')
+    // ->group(function () {
+    //     Route::get('/dashboard', [AdminDashboardController::class, 'index'])
+    //         ->name('dashboard');
+    // });
 });
+
 
 require __DIR__.'/settings.php';
 
-// 検索用route
-Route::get('/search',function () {
-    return view('search');
-})->name('search');
 
-// 
+//
