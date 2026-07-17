ccadee9e13d47c8226f55b7e5a4ba186a280bdeb
Rxwestx <wexriexst13@gmail.com>
2026-07-17 23:36:02 +0900

    UserモデルにuserAnimesリレーションを追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -32,6 +32,10 @@ class User extends Authenticatable implements MustVerifyEmail
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    public function userAnimes(): HasMany
+    {
+        return $this->hasMany(UserAnime::class);
+    }
     /**
      * Get the attributes that should be cast.
      *
