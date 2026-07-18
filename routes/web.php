8b36d2707da368a30f77314d4ad3a6bd10903e56
Rxwestx <wexriexst13@gmail.com>
2026-07-18 23:07:10 +0900

    検索用routeを追加し、ダッシュボードのルートを整理

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,35 @@
 <?php
-
+use \Htp\App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
+// 検索用route
+Route::get('/search',function () {
+    return inertia('search');
+})->name('search');
+
+// ログイン・メール認証済みユーザー用Route
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-    // 検索用route
-    Route::get('/search',function () {
-        return inertia('search');
-    })->name('search');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+
+    // 感情タグ画面Route
+    Route::get('/emotion-tags', function () {
+        return inertia('emotion-tags');
+    })->name('emotion-tags.index');
+});
 
     // 管理者用route
     // Route::prefix('admin')
     // ->name('admin.')
-    // ->middleware('admin')
+    // ->middleware(['auth', 'verified', 'admin'])
     // ->group(function () {
-    //     Route::get('/dashboard', [AdminDashboardController::class, 'index'])
-    //         ->name('dashboard');
+    //     Route::get('/dashboard', [AdminReviewController::class, 'index'])
+    //         ->name('reviews.index');
     // });
-});
+
 
 
 require __DIR__.'/settings.php';
