842ded168e643a3f3e30845f09c7fe4191580d5a
Rxwestx <wexriexst13@gmail.com>
2026-07-22 16:48:21 +0900

    AnimeSearchControllerのindexメソッドを修正し、MyAnimeListServiceを依存性注入してアニメ検索機能を改善

--- app/Http/Controllers/AnimeSearchController.php
+++ app/Http/Controllers/AnimeSearchController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MyAnimeListService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class AnimeSearchController extends Controller
 {
-    public function index(Request $request): Response
+    public function index(
+        Request $request,
+        MyAnimeListService $myAnimeListService
+    ): Response
     {
+        $keyword = $request->string('keyword')->toString();
+        $animes = $keyword !==''
+        ? $myAnimeListService->searchAnime($keyword)
+        : [];
+
         return Inertia::render('search', [
-            'keyword' => $request->string('keyword')->toString(),
+            'keyword' => $keyword,
+            'animes' => $animes,
         ]);
     }
 }
