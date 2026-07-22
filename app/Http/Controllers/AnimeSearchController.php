6cab6e9b4af4ca2ed1270b2d3dbcaca802983244
Rxwestx <wexriexst13@gmail.com>
2026-07-22 15:25:48 +0900

    MyAnimeList APIサービスを追加し、アニメ検索機能を実装

---
+++ app/Http/Controllers/AnimeSearchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AnimeSearchController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        return Inertia::render('search', [
+            'keyword' => $request->string('keyword')->toString(),
+        ]);
+    }
+}
---
+++ app/Services/MyAnimeListService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+
+class MyAnimeListService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function searchAnime(string $keyword): array
+    {
+        $response = Http::withHeaders([
+            'X-MAL-CLIENT-ID' => config('services.myanimelist.client_id'),
+        ])->get('https://api.myanimelist.net/v2/anime', [
+            'q' => $keyword,
+            'limit' => 20,
+            'fields' => 'id,title,main_picture,start_date,genres',
+        ]);
+
+        $response->throw();
+        return $response->json('data', []);
+    }
+}
