bf3111649d967a6991a5d51ecf673aa92a74b8bc
Rxwestx <wexriexst13@gmail.com>
2026-07-24 01:51:29 +0900

    UserAnimeControllerのstoreメソッドにアニメのステータスをバリデーションし、デバッグ出力を改善

--- app/Http/Controllers/UserAnimeController.php
+++ app/Http/Controllers/UserAnimeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\WatchingStatus;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class UserAnimeController extends Controller
@@ -10,11 +12,15 @@ class UserAnimeController extends Controller
     {
         $validated = $request->validate([
             'anime_id' => ['required', 'integer'],
+            'status' => ['required', Rule::enum (WatchingStatus::class)] ,
         ]);
 
         $anime = app(\App\Services\MyAnimeListService::class)
         ->getAnimeByMalId($validated['anime_id']);
 
-        dd($anime);
+        dd([
+            'anime' => $anime,
+            'status' => $validated['status'],
+        ]);
     }
 }
