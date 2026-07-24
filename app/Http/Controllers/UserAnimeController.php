71401cff5d6debab58b60abd83084cb0a4cb3e71
Rxwestx <wexriexst13@gmail.com>
2026-07-24 16:18:32 +0900

    UserAnimeControllerのstoreメソッドを修正し、MyAnimeListServiceを依存性注入で取得するように変更

--- app/Http/Controllers/UserAnimeController.php
+++ app/Http/Controllers/UserAnimeController.php
@@ -3,24 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Enums\WatchingStatus;
+use App\Models\AnimeMaster;
+use App\Models\UserAnime;
+use App\Services\MyAnimeListService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class UserAnimeController extends Controller
 {
-    public function store(Request $request)
-    {
+    public function store(
+        Request $request,
+        MyAnimeListService $myAnimeListService,
+    ):RedirectResponse{
         $validated = $request->validate([
             'anime_id' => ['required', 'integer'],
             'status' => ['required', Rule::enum (WatchingStatus::class)] ,
         ]);
 
-        $anime = app(\App\Services\MyAnimeListService::class)
+        $anime = $myAnimeListService
         ->getAnimeByMalId($validated['anime_id']);
 
-        dd([
-            'anime' => $anime,
+        $animeMaster = AnimeMaster::firstOrCreate(
+            ['mal_id' => $anime['id']],
+            [
+                'title' => $anime['title'],
+                'cover_image' => $anime['main_picture']['large']
+                ?? $anime['main_picture']['medium'] ?? null,
+                'description' => $anime['synopsis'] ?? null,
+                'genre' => collect($anime['genres'] ?? [])
+                ->pluck('name')
+                ->implode(', '),
+                'source' => $anime['source'] ?? null,
+                'num_episodes' => $anime['num_episodes'] ?? null,
+                'broadcast_year' => $anime['broadcast_year'] ?? null,
+            ]
+        );
+
+        $userAnime = UserAnime::firstOrCreate(
+            [
+            'user_id' => $request->user()->id,
+            'anime_master_id' => $animeMaster->id,
+            ],
+            [
             'status' => $validated['status'],
-        ]);
+            ],
+        );
+
+        if (!$userAnime->wasRecentlyCreated) {
+            return back()->withErrors([
+                'anime' => 'このアニメ作品はすでに登録されています。',
+            ]);
+        }
+        return back()->with('success', 'アニメ作品を登録しました！');
     }
 }
