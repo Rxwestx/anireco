6d100e0c63787b5d2579995afb7f3a13c73c35a1
Rxwestx <wexriexst13@gmail.com>
2026-07-26 09:47:00 +0900

    AnimeControllerにユーザーのアニメ登録情報を取得する機能を追加し、表示内容を改善

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserAnime;
 use App\Services\MyAnimeListService;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -10,13 +12,29 @@ use Inertia\Response;
 class AnimeController extends Controller
 {
     public function show(
+        Request $request,
         int $malId,
-         MyAnimeListService $myAnimeListService
+         MyAnimeListService $myAnimeListService,
          ): Response{
         $anime = $myAnimeListService->getAnimeByMalId($malId);
 
+        $userAnime = null;
+
+        if ($request->user()) {
+            $userAnime = UserAnime::query()
+                ->where('user_id', $request->user()->id)
+                ->whereHas('animeMaster', function ($query) use ($malId) {
+                    $query->where('mal_id', $malId);
+                })
+                ->first();
+        }
+        
         return Inertia::render('animes/show', [
-            'anime' => $anime,
+            'anime' =>[
+                ...$anime,
+                'user_anime_id' => $userAnime?->id,
+                'registered_status' => $userAnime?->status->value,
+            ],
         ]);
     }
 }
