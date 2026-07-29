197615ca79548831ee8402a7f728fa99e86f46e9
Rxwestx <wexriexst13@gmail.com>
2026-07-29 15:21:41 +0900

    Add: UserAnimeTagControllerを作成し、感情タグの登録機能を実装

---
+++ app/Http/Controllers/UserAnimeTagController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserAnime;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class UserAnimeTagController extends Controller
+{
+    public function store(
+        Request $request,
+        UserAnime $userAnime
+    ): RedirectResponse{
+        abort_unless(
+            $userAnime->user_id === $request->user()->id,
+            403,
+        );
+
+        $validated = $request->validate([
+            'emotion_tag_id' => [
+                'required',
+                'integer',
+            ],
+        ]);
+
+        $emotionTag = $request->user()
+            ->emotionTags()
+            ->findOrFail($validated['emotion_tag_id']);
+
+
+        $userAnime->emotionTags()
+            ->syncWithoutDetaching([
+                $emotionTag->id,
+            ]);
+
+        return back()->with(
+            'success',
+            '感情タグを登録しました。',
+        );
+    }
+}
