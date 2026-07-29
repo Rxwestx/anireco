eb167c32cc6eb51427fcd62519c6cc73eeb46e17
Rxwestx <wexriexst13@gmail.com>
2026-07-29 15:45:05 +0900

    Add: ユーザーアニメ感情タグ登録用routeを追加し、ルートの整形を改善

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\UserAnimeTagController;
 use App\Http\Controllers\EmotionTagController;
 use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\AnimeSearchController;
@@ -11,23 +12,25 @@ Route::inertia('/', 'welcome')->name('home');
 
 // 公開：検索用route
 Route::get('/search', [AnimeSearchController::class, 'index'])
-->name('anime.search');
+    ->name('anime.search');
 
 // 公開：アニメ詳細画面route
 Route::get('/animes/{malId}', [AnimeController::class, 'show'])
-->name('anime.show');
+    ->name('anime.show');
 
 // ログイン・メール認証済みユーザー用Route
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    // ユーザーアニメ登録用route
+    // 作品登録用route
     Route::post('/user-animes', [UserAnimeController::class, 'store'])
-    ->name('user-animes.store');
+        ->name('user-animes.store');
 
-    Route::patch('/user-animes/{userAnime}', [UserAnimeController::class, 'update'])
-        ->name('user-animes.update');
+    Route::patch(
+        '/user-animes/{userAnime}',
+        [UserAnimeController::class, 'update']
+    )->name('user-animes.update');
 
 
     // 感情タグ画面Route
@@ -39,13 +42,20 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::patch(
         '/emotion-tags/{emotionTag}',
-        [EmotionTagController::class, 'update'])
-        ->name('emotion-tags.update');
+        [EmotionTagController::class, 'update']
+    )->name('emotion-tags.update');
 
     Route::delete(
         '/emotion-tags/{emotionTag}',
-        [EmotionTagController::class, 'destroy'])
-        ->name('emotion-tags.destroy');
+        [EmotionTagController::class, 'destroy']
+    )->name('emotion-tags.destroy');
+
+    // ユーザーアニメ感情タグ登録用route
+    Route::post(
+        '/user-animes/{userAnime}/emotion-tags',
+        [UserAnimeTagController::class, 'store']
+    )->name('user-anime-tags.store');
+
 });
 
     // 管理者用route
