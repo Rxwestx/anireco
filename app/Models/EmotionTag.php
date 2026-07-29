3f4adadb5def5b73db3f9ae80d2a3bba77654ec5
Rxwestx <wexriexst13@gmail.com>
2026-07-29 15:21:35 +0900

    Add: EmotionTagモデルにuserAnimeTagsおよびuserAnimesリレーションを追加

--- app/Models/EmotionTag.php
+++ app/Models/EmotionTag.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class EmotionTag extends Model
 {
-    use HasFactory;
 
     protected $fillable = [
         'user_id',
@@ -16,5 +17,20 @@ class EmotionTag extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function userAnimeTags(): HasMany
+    {
+        return $this->hasMany(UserAnimeTag::class);
+    }
+
+    public function userAnimes(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            UserAnime::class,
+            'user_anime_tags',
+            'emotion_tag_id',
+            'user_anime_id'
+        )->withTimestamps();
     }
 }
