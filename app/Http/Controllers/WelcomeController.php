c8fce48cb847601ff593ae0258f41969eef7a24b
Rxwestx <wexriexst13@gmail.com>
2026-08-08 08:30:02 +0900

    Add: ユーザー登録アニメの状態を季節アニメリストに追加する機能を実装

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\UserAnime;
 use App\Services\MyAnimeListService;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -32,6 +33,48 @@ class WelcomeController extends Controller
             0,
             10,
         );
+
+        $registeredUserAnimes = collect();
+
+        if (request()->user()) {
+            $malIds = collect($seasonalAnime)
+            ->pluck('id')
+            ->unique()
+            ->values();
+
+            $registeredUserAnimes = UserAnime::query()
+                ->join(
+                    'anime_masters',
+                    'user_animes.anime_master_id',
+                    '=',
+                    'anime_masters.id',
+                )
+                ->where(
+                    'user_animes.user_id',
+                    request()->user()->id
+                )
+                ->whereIn('anime_masters.mal_id', $malIds)
+                ->get([
+                    'user_animes.id',
+                    'user_animes.status',
+                    'anime_masters.mal_id',
+                ])
+                ->keyBy('mal_id');
+        }
+
+        // 今季アニメのリストに登録状態を追加
+        $seasonalAnime = collect($seasonalAnime)
+            ->map(function (array $anime) use ($registeredUserAnimes) {
+                $registeredUserAnime = $registeredUserAnimes->get($anime['id']);
+
+                return [
+                    ...$anime,
+                    'user_anime_id' => $registeredUserAnime?->id,
+                    'registered_status' => $registeredUserAnime?->status->value,
+                ];
+            })
+            ->values()
+            ->all();
 
         return Inertia::render('welcome',[
             'seasonalAnime' => $seasonalAnime,
