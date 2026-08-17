edbee5ba9f6b48f2e21ca69415ade32c5adf324e
Rxwestx <wexriexst13@gmail.com>
2026-08-18 07:34:46 +0900

    Add: メールテンプレートのHTMLおよびテキストバージョンを作成

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<span style="font-size:24px; font-weight: 700; color: #111111">
+    AniReco
+</span>
+</a>
+</td>
+</tr>
