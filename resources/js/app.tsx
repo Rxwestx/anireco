ea66aa88445d8b9c6ddc73e40fca55b90c6cbf74
Rxwestx <wexriexst13@gmail.com>
2026-08-25 13:52:08 +0900

    修正: アプリ名を含むタイトルのフォーマットを改善

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -9,7 +9,7 @@ import SettingsLayout from '@/layouts/settings/layout';
 const appName = import.meta.env.VITE_APP_NAME || 'Anireco';
 
 createInertiaApp({
-    title: (title) => (title ? `${title}` : appName),
+    title: (title) => (title ? `${title} -${appName} ` : appName),
     layout: (name) => {
         switch (true) {
             case name === 'welcome':
